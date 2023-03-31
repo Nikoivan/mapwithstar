df2bd759c236b10686e254cb3246ec0b1a481e0c
Ivan Nikolaenko <nikolaenkostudio@gmail.com>
2023-03-31 12:43:12 +0300

    task had been final corrected

--- src/js/app.js
+++ src/js/app.js
@@ -1,48 +1,38 @@
 class Settings {
   constructor() {
-    this.defaultSettings = new Map();
-    this.userSettings = new Map();
     this.availableSettings = {
-      theme: ['dark', 'ligth', 'gray'],
-      music: ['trance', 'pop', 'rock', 'chillout', 'off'],
-      difficulty: ['easy', 'normal', 'hard', 'nigthmare'],
+      theme: ["dark", "ligth", "gray"],
+      music: ["trance", "pop", "rock", "chillout", "off"],
+      difficulty: ["easy", "normal", "hard", "nigthmare"],
     };
-
-    this.defaultSettings.set('theme', 'dark');
-    this.defaultSettings.set('music', 'trance');
-    this.defaultSettings.set('difficulty', 'easy');
   }
 
   setSettings(settings) {
+    this.userSettings = new Map([
+      ["theme", "dark"],
+      ["music", "trance"],
+      ["difficulty", "easy"],
+    ]);
     const test = Object.entries(settings);
-    test.forEach((el) => {
+    test.forEach(([title, description]) => {
       if (
-        !(el[0] in this.availableSettings)
-        || !this.availableSettings[el[0]].includes(el[1])
+        !(title in this.availableSettings) ||
+        !this.availableSettings[title].includes(description)
       ) {
-        throw new Error('Ошибка. Вы ввели недопустимые значения настроек');
+        throw new Error("Ошибка. Вы ввели недопустимые значения настроек");
       }
-      this.userSettings.set(el[0], el[1]);
+      this.userSettings.set(title, description);
     });
   }
 
   getSettings() {
-    const finalSettings = new Map();
-    const defaultKeys = ['theme', 'music', 'difficulty'];
-    defaultKeys.forEach((el) => {
-      if (this.userSettings.has(el)) {
-        finalSettings.set(el, this.userSettings.get(el));
-      } else {
-        finalSettings.set(el, this.defaultSettings.get(el));
-      }
-    });
-    return finalSettings;
+    return this.userSettings;
   }
 }
 
 export default Settings;
 
 const settings = new Settings();
-settings.setSettings({ theme: 'dark', music: 'trance' });
+settings.setSettings({ theme: "dark", music: "trance" });
 const result = settings.getSettings();
 console.log(result);
--- test/app.test.js
+++ test/app.test.js
@@ -1,30 +1,30 @@
-import Settings from '../src/js/app';
+import Settings from "../src/js/app";
 
-test('test throwError of method setSettings by incorrect title of setting', () => {
+test("test throwError of method setSettings by incorrect title of setting", () => {
   const settings = new Settings();
 
   expect(() => {
-    settings.setSettings({ sheme: 'dark', music: 'trance' });
-  }).toThrow('Ошибка. Вы ввели недопустимые значения настроек');
+    settings.setSettings({ sheme: "dark", music: "trance" });
+  }).toThrow("Ошибка. Вы ввели недопустимые значения настроек");
 });
 
-test('test throwError of method setSettings by incorrect value of setting', () => {
+test("test throwError of method setSettings by incorrect value of setting", () => {
   const settings = new Settings();
 
   expect(() => {
-    settings.setSettings({ theme: 'dar', music: 'trance' });
-  }).toThrow('Ошибка. Вы ввели недопустимые значения настроек');
+    settings.setSettings({ theme: "dar", music: "trance" });
+  }).toThrow("Ошибка. Вы ввели недопустимые значения настроек");
 });
 
-test('test for method setSettings create new userSettings', () => {
+test("test for method setSettings create new userSettings", () => {
   const settings = new Settings();
-  settings.setSettings({ theme: 'dark', music: 'trance' });
-  expect(settings.userSettings.size).toBe(2);
+  settings.setSettings({ theme: "dark", music: "trance" });
+  expect(settings.userSettings.size).toBe(3);
 });
 
-test('test for method getSetting get a finalSettings', () => {
+test("test for method getSetting get a finalSettings", () => {
   const settings = new Settings();
-  settings.setSettings({ theme: 'dark', music: 'trance' });
+  settings.setSettings({ theme: "dark", music: "trance" });
   const result = settings.getSettings();
   expect(result.size).toBe(3);
 });
